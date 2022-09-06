b93aa1ce4fc8650889a503ddcb3ef65ace9454c9
Ricardo <ricardomartinso.dev@gmail.com>
2022-09-06 11:45:10 -0300

    starting base structure

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+
+export default function errorHandler(
+  error: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (error) {
+    if (error.error == "NotFound") {
+      return res.status(404).send(error.message);
+    }
+    if (error.error == "Conflict") {
+      return res.status(409).send(error.message);
+    }
+    if (error.error == "NotAuthorized") {
+      return res.status(422).send(error.message);
+    }
+    if (error.error == "Unauthorized") {
+      return res.status(401).send(error.message);
+    }
+  }
+
+  return res.status(500).send("Server error has ocurred!");
+}
